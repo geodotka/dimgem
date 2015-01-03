2ffb8210088f065eb8fc8b49f3eeead46e50f08f
Dorota Kowalik <dorota.kowalik@21s.pl>
2015-01-04 00:22:30 +0100

    #29: change admin panel for NoteToPost

--- dimgem/admin.py
+++ dimgem/admin.py
@@ -6,7 +6,14 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'posted_date', 'author', 'text', 'dim',
                     'categories', 'old_text')
 
+
+class NoteToPostAdmin(admin.ModelAdmin):
+    list_display = ('post_id', 'author', 'anon_author', 'text',
+                    'submited_date', 'is_accepted', 'accept_date',
+                    'accept_superuser')
+
+
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Vote)
-admin.site.register(NoteToPost)
+admin.site.register(NoteToPost, NoteToPostAdmin)
